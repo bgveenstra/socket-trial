2cefb8fe5c6fca03132cf7fab02db790e9dd17bc
Brianna Veenstra <b.g.veenstra@gmail.com>
2015-09-03 22:34:26 -0700

    tested, simple

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,41 +4,66 @@ console.log('app.js');
 // console.log('io ', io);
 var app = angular.module('socketApp', ['ngRoute']);
 
-app.factory('socketService', ['$rootScope', '$log', function($rootScope, $log){
-	var socket = io.connect();
+app.factory('socketService', ['$rootScope', '$log', function ($rootScope, $log) {
+    var socket = io.connect();
 	$log.log('socket - client connection created');
-
-	return {
-		on: function(eventName, callback) {
-			socket.on(eventName, callback);
-			return function(){
-				$log.log('socket - received event with name ', eventName);
-			}
-		},
-		emit: function(eventName, data, callback){
-			socket.emit(eventName, data, function(){
-				$log.log('inside emit');
-			})
-			$log.log('socket - sending event with name ', eventName, '\ndata ', data);
-		}
-	}
+ 
+    return {
+        on: function (eventName, callback) {
+	        	$log.log('socket - received event with name ', eventName);
+	        	// set up wrapper function to synch socket changes with angular
+	            function wrapper() {
+	            	// store the arguments object 
+	            	// which just contains the function arguments
+	                var args = arguments;
+	                console.log('on args: ',args);
+	                // use $apply to trigger angular 2way data binding update
+	                $rootScope.$apply(function () {
+	                    callback.apply(socket, args);
+	                });
+	            }
+	 
+	 			// calls uses socket.io's on method with 
+	 			// our callback function and the $apply wrapper that helps synch up with angular 
+	            socket.on(eventName, wrapper);
+	 
+	 			// @TODO - why?
+	            // return function () {
+	            // 	$log.log("superfun")
+	            //     socket.removeListener(eventName, wrapper);
+	            // };
+	        },
+ 
+        emit: function (eventName, data, callback) {
+	        	$log.log('socket - sending event with name ', eventName, '\ndata ', data);
+	            socket.emit(eventName, data, function () {
+	                var args = arguments;
+	                //console.log('emit args: ',args);
+	                $rootScope.$apply(function () {
+	                    if(callback) {
+	                        callback.apply(socket, args);
+	                    }
+	                });
+	            });
+	        }
+    };
 }]);
 
+
 app.controller('MainCtrl', ['$scope', '$log', 'socketService', function($scope, $log, socketService){
-	$scope.test = "greetings!";
-	$scope.messageLog = ["first!"];
-	$scope.newMessage = "";
+	$scope.test = 'greetings!';
+	$scope.messageLog = [{message: 'first!', time: 0, sender: 'epoch'}];
+	$scope.newMessage = '';
 	$scope.sendThroughSocket = function(msg){
 		socketService.emit('message-event', msg);
+		$scope.messageLog.push({message: msg, time: Date.now(), sender: 'me'});
 	}
-	$log.log("socketService", socketService)
 	socketService.on('greeting-event', function(data){
 		$log.log('received greeting with data ', data);
 	});
 	socketService.on('communication-event', function(data){
 		$log.log('received communication with data ', data);
-		$scope.messageLog.push(">> "+data.msg);
-		$log.log($scope.messageLog);
+		$scope.messageLog.push({message: data.msg, time: Date.now(), sender: 'them'});
 	});
 }]);
 
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var express = require('express'),
 app.use(express.static(__dirname + '/public'));
 
 
+// api routes go here
 
+// catchall route for angular front end
 app.get('*', function(req, res){
 	res.sendFile(__dirname + '/public/views/index.html');
 })
@@ -25,9 +27,20 @@ var io = require('socket.io').listen(httpServer);
 // console.log('io', io);
 io.on('connection', function(socket){
 	console.log('new connection');
+	console.log(socket);
+	// socket.emit sends a message across one connection 
+	// (to one client user)
 	socket.emit('greeting-event', {msg: 'hello new user!'});
+	// io.emit sends the message to everyone
+	io.emit('communication-event', {msg: 'a user joined'});
 	socket.on('message-event', function(data){
 		console.log('received data: ', data);
-		socket.emit('communication-event', {msg: data});
-	});
+		// socket.broadcast.emit sends a message to everyone but
+		// the client
+  		socket.broadcast.emit('communication-event', {msg: data})
+		console.log('sent communication-event')
+	});  
+	socket.on('disconnect', function(){
+    	console.log('user disconnected');
+  });
 });
